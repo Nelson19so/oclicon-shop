30d882bd0869d95bafd1971fb9b849e7645b9196
Nelson <nelsonsomto10@gmail.com>
2025-05-19 17:31:49 +0100

    Add migrations for accounts and public apps

--- build.sh
+++ build.sh
@@ -8,5 +8,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input --settings=settings.settings.prod
 
 # Apply all migrations for production
-python manage.py migrate accounts --no-input --settings=settings.settings.prod
 python manage.py migrate --no-input --settings=settings.settings.prod
