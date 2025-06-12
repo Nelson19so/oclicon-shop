7415b08964d9550c899476886158e6f6922a9d78
Nelson <nelsonsomto10@gmail.com>
2025-06-13 00:38:03 +0100

    debbuging server error 500

--- build.sh
+++ build.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# exit on error & print commands
+set -ex
+
 # Install dependencies
 pip install -r requirements.txt
 
 # Collect static files for production
 python manage.py collectstatic --no-input --settings=settings.settings.prod
 
+# create new migration files
+python manage.py makemigrations
+
 # Apply all migrations for production
 python manage.py migrate --no-input --settings=settings.settings.prod
+
