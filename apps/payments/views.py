96ab8423e85305aaec653bef266c72ebcc0022a7
Nelson <nelsonsomto10@gmail.com>
2025-06-24 23:58:14 +0100

    updated payment app view

--- apps/payments/views.py
+++ apps/payments/views.py
@@ -7,6 +7,7 @@ from django.views import View
 from django.utils import timezone
 from django.conf import settings
 from django.http import HttpResponse
+from apps.orders.models import Order
 
 class StartOrderPayment(View):
     @method_decorator(require_POST)
@@ -14,25 +15,32 @@ class StartOrderPayment(View):
         user = request.user
 
         reference = f"TXN{user.id}{timezone.now().strftime('%Y%m%d%H%M%S')}"
-        total_amount = 500
-        TempPayment.objects.create(user=user, reference=reference, amount=total_amount)
-        
-        headers = {
-            "Authorization": f"Bearer {settings.PAYSTACK_SECRET_KEY}",
-            "Content-Type": "application/json"
-        }
 
-        data = {
-            "email": user.email,
-            "amount": int(total_amount * 100),  # in kobo
-            "reference": reference,
-            "callback_url": "http://localhost:8000/payments/verify/",
-        }
-        
-        res = requests.post("https://api.paystack.co/transaction/initialize", json=data, headers=headers)
-        auth_url = res.json()['data']['authorization_url']
+        try:
+            order = Order.objects.filter(user=user)
+            order_total_amount = order.total_amount
 
-        return redirect(auth_url)
+            total_amount = order_total_amount
+            TempPayment.objects.create(user=user, reference=reference, amount=total_amount)
+            
+            headers = {
+                "Authorization": f"Bearer {settings.PAYSTACK_SECRET_KEY}",
+                "Content-Type": "application/json"
+            }
+
+            data = {
+                "email": user.email,
+                "amount": int(total_amount * 100),  # in kobo
+                "reference": reference,
+                "callback_url": "http://localhost:8000/payments/verify/",
+            }
+            
+            res = requests.post("https://api.paystack.co/transaction/initialize", json=data, headers=headers)
+            auth_url = res.json()['data']['authorization_url']
+
+            return redirect(auth_url)
+        except Order.DoesNotExist:
+            return redirect('home')
 
 class VerifyPaymentOrderView(View):
     @method_decorator(require_GET)
@@ -49,7 +57,9 @@ class VerifyPaymentOrderView(View):
 
         if result['data']['status'] == 'success':
             pass
-        
+            # logics goes here but since we are still working on it, we will use pass method
+            # this method processes wen the payment is successful
+
             return HttpResponse("✅ Payment successful and order created.")
 
         return HttpResponse("❌ Payment failed.")
