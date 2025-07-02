a1f5ccfaf717b3ea8573599c0be4bd576740d921
Nelson <nelsonsomto10@gmail.com>
2025-07-02 22:08:49 +0100

    upadted product test model

--- apps/products/tests/test_model.py
+++ apps/products/tests/test_model.py
@@ -1,8 +1,20 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from apps.products.models import Brand, Category
+from django.contrib.auth import get_user_model
 
-# test category model
+User = get_user_model()
+
+# testing all product model for product app
 class ProductsModelTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create(
+            username='tesUser', email='testemail@gmail.com', password='testpassword_12.'
+        )
+        self.logged_in = self.client.login(
+            email='testemail@gmail.com', password='testpassword_12.'
+        )
+
     def test_products_models(self):
         brand = Brand.objects.create(name='testBrand')
         category = Category.objects.create(
