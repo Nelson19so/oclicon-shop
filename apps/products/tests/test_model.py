a358499260232d13c77519f2c7718ea12dd4befc
Nelson <nelsonsomto10@gmail.com>
2025-07-02 21:56:40 +0100

    updated test_model for products

--- apps/products/tests/test_model.py
+++ apps/products/tests/test_model.py
@@ -1,9 +1,9 @@
 from django.test import TestCase
 from apps.products.models import Brand, Category
 
-# test Brand model
-class ProductModelTest(TestCase):
-    def product_model_test(self):
+# test category model
+class ProductsModelTest(TestCase):
+    def test_products_models(self):
         brand = Brand.objects.create(name='testBrand')
         category = Category.objects.create(
             name='testCategory'
@@ -12,8 +12,6 @@ class ProductModelTest(TestCase):
             name='testCategoryChild',
             parent=category
         )
+        self.assertEqual(str(brand), 'testBrand')
         self.assertEqual(str(category), 'testCategory')
         self.assertEqual(str(children), 'testCategoryChild')
-        self.assertEqual(str(brand), 'testBrand')
-
-        
