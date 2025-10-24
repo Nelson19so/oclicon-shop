9328b48fd6ac2b8c544f72ce08805e8ddf1d3912
Nelson <nelsonsomto10@gmail.com>
2025-10-24 18:57:48 +0100

    integrated cloudianry

--- src/config/settings/prod.py
+++ src/config/settings/prod.py
@@ -15,30 +15,27 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
-        #'OPTIONS': {'sslmode': 'require'},
-        #'CONN_MAX_AGE': 600,
+        # 'OPTIONS': {'sslmode': 'require'},
+        # 'CONN_MAX_AGE': 600,
     }
 }
 
 # --- CLOUDINARY CONFIGURATION ---
 # CLOUDINARY_URL = os.getenv('CLOUDINARY_URL')
+MEDIA_URL = '/media/'
 
 cloudinary.config(
     cloud_name=os.getenv('CLOUD_NAME'),
     api_key=os.getenv('CLOUD_API_KEY'),
     api_secret=os.getenv('CLOUD_API_SECRET'),
 )
+
 # Use Cloudinary for media uploads
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-# MEDIA_URL = 'https://res.cloudinary.com/%s/' % os.getenv('CLOUD_NAME')
 
-# --- MEDIA & STATIC ---
+# --- STATIC ---
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # --- ALLOWED HOSTS ---
 ALLOWED_HOSTS = ['oclicon-shop.onrender.com']
 
-"""
-This prod.py config connects Django media uploads to Cloudinary CDN for production.
-Uploaded images will appear in your Cloudinary dashboard automatically.
-"""
