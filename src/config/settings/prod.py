fbeeeb54aeaad14868724a8c7297a4ae92fac4c7
Nelson <nelsonsomto19@gmail.com>
2026-09-11 16:22:59 +0100

    added production supabase pooler url

--- src/config/settings/prod.py
+++ src/config/settings/prod.py
@@ -7,26 +7,38 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-key-for-vercel-buildi
 
 DEBUG = False 
 
-# DATABASE CONFIGURATION
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL', 'postgres://localhost/dummy_db'),
-        conn_max_age=600
-    )
-}
+db_from_env = dj_database_url.config(conn_max_age=600)
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('DB_NAME'),
-#         'USER': os.getenv('DB_USER'),
-#         'PASSWORD': os.getenv('DB_PASSWORD'),
-#         'HOST': os.getenv('DB_HOST'),
-#         'PORT': os.getenv('DB_PORT'),
-#         # 'OPTIONS': {'sslmode': 'require'},
-#         # 'CONN_MAX_AGE': 600,
-#     }
-# }
+if db_from_env:
+    DATABASES = {
+        'default': db_from_env
+    }
+else:
+    DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.postgresql',
+                'NAME': 'postgres',
+                'USER': 'dyh5dzke6',
+                'PASSWORD': 'k@UM?*s.X*64/hY',
+                'HOST': 'localhost',
+                'PORT': '5432',
+                # 'OPTIONS': {'sslmode': 'require'},
+                # 'CONN_MAX_AGE': 600,
+            }
+        }
+    # DATABASES = {
+    #     'default': {
+    #         'ENGINE': 'django.db.backends.postgresql',
+    #         'NAME': os.getenv('DB_NAME'),
+    #         'USER': os.getenv('DB_USER'),
+    #         'PASSWORD': os.getenv('DB_PASSWORD'),
+    #         'HOST': 'localhost',
+    #         'PORT': '5432',
+    #         # 'OPTIONS': {'sslmode': 'require'},
+    #         # 'CONN_MAX_AGE': 600,
+    #     }
+    # }
+
 
 cloudinary.config(
     cloud_name=os.getenv('CLOUD_NAME'),
