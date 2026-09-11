ca32c625165ef2793ae0923816b44482562c6a32
Nelson <nelsonsomto19@gmail.com>
2026-09-11 20:31:11 +0100

    fix vercel django python path

--- src/config/wsgi.py
+++ src/config/wsgi.py
@@ -2,13 +2,15 @@ import os
 import sys
 from pathlib import Path
 
-# /var/task/src
 SRC_DIR = Path(__file__).resolve().parent.parent
 
 if str(SRC_DIR) not in sys.path:
     sys.path.insert(0, str(SRC_DIR))
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.config.settings.prod')
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "src.config.settings.prod"
+)
 
 from django.core.wsgi import get_wsgi_application
 
