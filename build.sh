8e97ab5f1acb795710b0316112d1571e1d220819
Nelson <nelsonsomto19@gmail.com>
2026-09-11 15:42:42 +0100

    updated the settings and build.sh

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@ set -o errexit
 set -ex  # Print commands as they run and exit on failure
 
 # Install dependencies
-pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
-# Apply DB migrations (apply)
-# python manage.py migrate --no-input --settings=src.config.settings.prod
+# Create staticfiles directory explicitly to ensure it exists
+mkdir -p staticfiles
 
-# Collect static files
-python manage.py collectstatic --no-input --settings=src.config.settings.prod
+# Collect static files using your specific configuration path
+python3 manage.py collectstatic --no-input --settings=src.confi.settings.prod
 
 echo "Build phase complete."
