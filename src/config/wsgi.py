baefcc2eb23d6f04967d2fe110baebc226005b1e
Nelson <nelsonsomto19@gmail.com>
2026-09-11 20:25:02 +0100

    fix vercel python path

--- src/config/wsgi.py
+++ src/config/wsgi.py
@@ -1,22 +1,15 @@
 import os
 import sys
 from pathlib import Path
-from django.core.wsgi import get_wsgi_application
 
-# Calculate the root folder path (where manage.py lives)
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+# /var/task/src
+SRC_DIR = Path(__file__).resolve().parent.parent
 
-# Force Python to look directly inside the project root for modules
-if str(BASE_DIR) not in sys.path:
-    sys.path.insert(0, str(BASE_DIR))
-
-# force Python to look directly inside 'src' for cleaner sub-app mappings
-SRC_DIR = BASE_DIR / 'src'
 if str(SRC_DIR) not in sys.path:
     sys.path.insert(0, str(SRC_DIR))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.config.settings.prod')
 
-application = get_wsgi_application()
+from django.core.wsgi import get_wsgi_application
 
-app = application
+application = get_wsgi_application()
