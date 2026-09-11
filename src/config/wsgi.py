11cb76c4485c1041fb595717e5d13a4ac444dbcc
Nelson <nelsonsomto19@gmail.com>
2026-09-11 16:51:07 +0100

    added pythonpath environment layout to vercel routing schema

--- src/config/wsgi.py
+++ src/config/wsgi.py
@@ -1,17 +1,8 @@
 import os
-import sys
-from pathlib import Path
 from django.core.wsgi import get_wsgi_application
-
-# Calculate the path to the root folder (where manage.py sits)
-# and ensure it's in Python's lookup registry
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
-if str(BASE_DIR) not in sys.path:
-    sys.path.insert(0, str(BASE_DIR))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.config.settings.prod')
 
 application = get_wsgi_application()
 
-# Hook for Vercel's wrapper interface
 app = application
