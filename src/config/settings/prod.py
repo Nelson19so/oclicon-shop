eed9a68a6343a9c029c61b04de25806319e3e94b
Nelson <nelsonsomto19@gmail.com>
2026-09-11 16:25:10 +0100

    added production supabase pooler url

--- src/config/settings/prod.py
+++ src/config/settings/prod.py
@@ -7,37 +7,32 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-key-for-vercel-buildi
 
 DEBUG = False 
 
-db_from_env = dj_database_url.config(conn_max_age=600)
+# db_from_env = dj_database_url.config(conn_max_age=600)
 
-if db_from_env:
-    DATABASES = {
-        'default': db_from_env
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'dyh5dzke6',
+        'PASSWORD': 'k@UM?*s.X*64/hY',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        # 'OPTIONS': {'sslmode': 'require'},
+        # 'CONN_MAX_AGE': 600,
     }
-else:
-    DATABASES = {
-            'default': {
-                'ENGINE': 'django.db.backends.postgresql',
-                'NAME': 'postgres',
-                'USER': 'dyh5dzke6',
-                'PASSWORD': 'k@UM?*s.X*64/hY',
-                'HOST': 'localhost',
-                'PORT': '5432',
-                # 'OPTIONS': {'sslmode': 'require'},
-                # 'CONN_MAX_AGE': 600,
-            }
-        }
-    # DATABASES = {
-    #     'default': {
-    #         'ENGINE': 'django.db.backends.postgresql',
-    #         'NAME': os.getenv('DB_NAME'),
-    #         'USER': os.getenv('DB_USER'),
-    #         'PASSWORD': os.getenv('DB_PASSWORD'),
-    #         'HOST': 'localhost',
-    #         'PORT': '5432',
-    #         # 'OPTIONS': {'sslmode': 'require'},
-    #         # 'CONN_MAX_AGE': 600,
-    #     }
-    # }
+}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#         # 'OPTIONS': {'sslmode': 'require'},
+#         # 'CONN_MAX_AGE': 600,
+#     }
+# }
 
 
 cloudinary.config(
