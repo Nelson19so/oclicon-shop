875cc4b50fdb953df610fcfcd76ae2c0789b820e
Nelson <nelsonsomto19@gmail.com>
2026-09-12 09:28:40 +0100

    include django apps in vercel function

--- build.sh
+++ build.sh
@@ -10,6 +10,6 @@ mkdir -p staticfiles
 
 # Collect static files targeting your specific production configuration path
 python3 manage.py collectstatic --no-input --settings=src.config.settings.prod
-python manage.py migrate
+python3 manage.py migrate
 
 echo "Build phase complete."
